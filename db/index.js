4e6731a66666fbb8c7810cb24f8ce98dcd8441cf
sknhshrmn <sakinahshahriman@gmail.com>
2023-06-22 21:09:12 +0800

    add ssl db

--- db/index.js
+++ db/index.js
@@ -7,6 +7,7 @@ const db = new Pool({
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
   database: process.env.DB_DATABASE || "my blog",
+  ssl: process.env.DB_SSL || false,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
