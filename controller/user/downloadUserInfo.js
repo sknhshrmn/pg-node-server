7c29d7bc3fabb996ae1429c5dd5d7e0a6a139e8c
sknhshrmn <sakinahshahriman@gmail.com>
2023-06-24 01:26:17 +0800

    revise download user info

--- controller/user/downloadUserInfo.js
+++ controller/user/downloadUserInfo.js
@@ -3,22 +3,20 @@ import axios from "axios";
 
 const downloadUserInfo = async (req, res) => {
   try {
-    const templateId = "d9ed224";
-    const apiKey = "m-D5iMRtMqQkkLGxvPw8d";
+    const templateId = "f1ece9b";
+    const apiKey = "Pu8XyXUYIAbdy-cCZPHlh";
     const apiUrl = `https://pdfgen.app/api/generate?templateId=${templateId}`;
-    const id = req.params.id;
-    const dbRes = await query("SELECT * FROM users WHERE id=$1", [id]);
-    // const requestData = dbRes.rows[0];
+
     const requestData = {
       data: {
-        id: 10,
-        username: "notadmin",
-        email: "notadmin@mail.com",
+        id: 1,
+        email: "admin@mail.com",
+        is_admin: true,
         password:
-          "$2a$10$Yf5CiJKTtNGcDxQSZv3hmu24Xt15VAdhgxMNJl6pKqxJtlRHsIFH2",
-        created_at: "2023-06-23T16:43:56.091Z",
+          "$2a$10$/ZaBZeSMB/COqY4j5hFHiOUtYFRSshwFwLDAxYGv2SYUSaHpSEGVu",
+        username: "admin",
+        created_at: "2023-06-10T01:35:43.836Z",
         deleted_at: null,
-        isadmin: false,
       },
     };
 
