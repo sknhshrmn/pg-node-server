6c21ba741db37a6c179fc509c12d09ef08ac7ebf
sknhshrmn <sakinahshahriman@gmail.com>
2023-06-24 00:58:50 +0800

    revise download user info

--- controller/user/downloadUserInfo.js
+++ controller/user/downloadUserInfo.js
@@ -8,7 +8,20 @@ const downloadUserInfo = async (req, res) => {
     const apiUrl = `https://pdfgen.app/api/generate?templateId=${templateId}`;
     const id = req.params.id;
     const dbRes = await query("SELECT * FROM users WHERE id=$1", [id]);
-    const requestData = dbRes.rows[0];
+    // const requestData = dbRes.rows[0];
+    const requestData = {
+      data: {
+        id: 1,
+        email: "admin@mail.com",
+        is_admin: true,
+        password:
+          "$2a$10$/ZaBZeSMB/COqY4j5hFHiOUtYFRSshwFwLDAxYGv2SYUSaHpSEGVu",
+        username: "admin",
+        created_at: "2023-06-10T01:35:43.836Z",
+        deleted_at: null,
+      },
+    };
+
     const response = await axios.post(apiUrl, requestData, {
       headers: {
         "Content-Type": "application/json",
@@ -21,15 +34,9 @@ const downloadUserInfo = async (req, res) => {
     res.setHeader("Content-Disposition", "attachment; filename=mypdf.pdf");
     console.log(response);
     res.send(response.data);
-  } catch {
-    (function (error) {
-      // handle error
-      // console.error(error);
-      // console.log(JSON.stringify(error));
-      if (error.response.status === 401) {
-        navigate("login");
-      }
-    });
+  } catch (error) {
+    console.error("Error generating PDF:", error.message);
+    res.status(500).send("Error generating PDF");
   }
 };
 
